7d42c6da417e810bc3a410919f509785d9591f56
LanDecs <eldrexdelosreyesbula@gmail.com>
2025-07-31 18:27:34 +0800

    Create config.js

---
+++ functions/config.js
@@ -0,0 +1,16 @@
+// public/config.js
+window.appConfig = {
+  firebase: {
+    apiKey: "AIzaSyCEBkAxkrm95b4LN-iNHmwXcpoLtwpQVlg",
+    authDomain: "countify-eb9dd.firebaseapp.com",
+    projectId: "countify-eb9dd",
+    storageBucket: "countify-eb9dd.appspot.com",
+    messagingSenderId: "112900990389",
+    appId: "1:112900990389:web:b2adbd5edbb658a27725fc",
+    measurementId: "G-LDBWQNNWG9"
+  },
+  paypal: {
+    clientId: "AYG_Ze1ITfxBvEb3QwW4F5BgdXThYisLcu_ZQTzgPeFRBsxp_yOGrjCGBxrqzvK4mKBZSK-VkhIMooPu",
+    env: "production"
+  }
+};
