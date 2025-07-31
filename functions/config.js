7a0ab9b55f4bd758f42f07b3d3d867d615e8296b
LanDecs <eldrexdelosreyesbula@gmail.com>
2025-07-31 18:46:40 +0800

    Update config.js

--- functions/config.js
+++ functions/config.js
@@ -1,16 +1,29 @@
+// config.js (in gitignored folder)
+window.appConfig = (function() {
+  // Private configuration
+  const secrets = {
+    firebase: {
+      apiKey: "AIzaSyCEBkAxkrm95b4LN-iNHmwXcpoLtwpQVlg",
+      authDomain: "countify-eb9dd.firebaseapp.com",
+      projectId: "countify-eb9dd",
+      storageBucket: "countify-eb9dd.appspot.com",
+      messagingSenderId: "112900990389",
+      appId: "1:112900990389:web:b2adbd5edbb658a27725fc",
+      measurementId: "G-LDBWQNNWG9"
+    },
+    paypal: {
+      clientId: "AYG_Ze1ITfxBvEb3QwW4F5BgdXThYisLcu_ZQTzgPeFRBsxp_yOGrjCGBxrqzvK4mKBZSK-VkhIMooPu",
+      currency: "PHP"
+    }
+  };
 
-window.appConfig = {
-  firebase: {
-    apiKey: "AIzaSyCEBkAxkrm95b4LN-iNHmwXcpoLtwpQVlg",
-    authDomain: "countify-eb9dd.firebaseapp.com",
-    projectId: "countify-eb9dd",
-    storageBucket: "countify-eb9dd.appspot.com",
-    messagingSenderId: "112900990389",
-    appId: "1:112900990389:web:b2adbd5edbb658a27725fc",
-    measurementId: "G-LDBWQNNWG9"
-  },
-  paypal: {
-    clientId: "AYG_Ze1ITfxBvEb3QwW4F5BgdXThYisLcu_ZQTzgPeFRBsxp_yOGrjCGBxrqzvK4mKBZSK-VkhIMooPu",
-    currency:"PHP"
-  }
-};
+  // Public interface
+  return {
+    getFirebaseConfig: () => ({ ...secrets.firebase }),
+    getPayPalConfig: () => ({ ...secrets.paypal }),
+    
+    // Safe methods
+    getPayPalClientId: () => secrets.paypal.clientId,
+    getCurrency: () => secrets.paypal.currency
+  };
+})();
