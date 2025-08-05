fa17af1932c6e4cf6e1ec06a5162543058a22880
LanDecs <eldrexdelosreyesbula@gmail.com>
2025-08-05 11:01:00 +0800

    Create config.js

---
+++ functions/config.js
@@ -0,0 +1,6 @@
+// config.js
+const config = {
+  CLOUDFLARE_API_TOKEN: 'your_cloudflare_api_token_here',
+  CLOUDFLARE_ACCOUNT_ID: 'your_cloudflare_account_id',
+  CLOUDFLARE_ZONE_ID: 'your_cloudflare_zone_id',
+};
