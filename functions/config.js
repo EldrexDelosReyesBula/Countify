efbb1f653485a402dc82b348615c151a28031b22
LanDecs <eldrexdelosreyesbula@gmail.com>
2025-08-05 11:24:47 +0800

    Update config.js

--- functions/config.js
+++ functions/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
   CLOUDFLARE_API_TOKEN: 'your_cloudflare_api_token_here',
-  CLOUDFLARE_ACCOUNT_ID: 'your_cloudflare_account_id',
-  CLOUDFLARE_ZONE_ID: 'your_cloudflare_zone_id',
+  CLOUDFLARE_ACCOUNT_ID: 'b84783e52b661c31ea3e2e25ecdad09b',
+  CLOUDFLARE_ZONE_ID: '97b6d759fba5c0dc44dfe0a793bb1544',
 };
