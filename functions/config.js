5a81746e84efe687be7eaf284f4087040d335dbe
Eldrex Delos Reyes Bula <eldrexdelosreyesbula@gmail.com>
2025-11-08 00:32:26 +0800

    Delete functions directory

--- functions/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const config = {
-  CLOUDFLARE_API_TOKEN: 'oXM8K5kYsiqM2S6FVhVsK6GgC7STl5bGhAUERVnb',
-  CLOUDFLARE_ACCOUNT_ID: 'b84783e52b661c31ea3e2e25ecdad09b',
-  CLOUDFLARE_ZONE_ID: '97b6d759fba5c0dc44dfe0a793bb1544',
-};
